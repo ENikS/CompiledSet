9906b162a37699c7bfc3b80040f20fddd79aa2c7
Eugene Sadovoi <evgeni@eniks.com>
2021-07-23 17:56:00 -0700

    Renaming assemblies

--- src/Analyzer/CompiledSetAnalyzer.cs
+++ src/Analyzer/CompiledSetAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using CompiledSet.Analyzer;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
